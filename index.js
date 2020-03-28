78657fec0dbbb5bb5e1f5a983cc316af5ff630e5
viralbpatel <patel.viral@husky.neu.edu>
2020-03-28 14:59:01 -0400

    Updates for Heroku Deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
+    /* response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); */
+    response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    next();
+});
+
+
+require('./controllers/quiz.controller.server')(app);
+require('./controllers/question.controller.server')(app);
+
+app.listen(PORT);
