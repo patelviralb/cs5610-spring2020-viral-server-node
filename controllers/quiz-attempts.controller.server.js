022630fde597fdbe79142c70514ec143ad2c691f
viralbpatel <patel.viral@husky.neu.edu>
2020-04-08 01:57:04 -0400

    Updated params in attempt controller

--- controllers/quiz-attempts.controller.server.js
+++ controllers/quiz-attempts.controller.server.js
@@ -2,12 +2,12 @@ const quizAttemptDao = require('../daos/quiz-attempts.dao.server');
 
 module.exports = (app) => {
     app.post('/api/quizzes/:quizId/attempts', (request, response) =>
-        quizAttemptDao.createAttempt(request.params.quizId, request.body)
+        quizAttemptDao.createAttempt(request.params['quizId'], request.body)
             .then(attempt => response.send(attempt))
     );
 
     app.get('/api/quizzes/:quizId/attempts', (request, response) =>
-        quizAttemptDao.findAttemptsForQuiz(request.params.quizId)
+        quizAttemptDao.findAttemptsForQuiz(request.params['quizId'])
             .then(attempts => response.send(attempts))
     );
 };
