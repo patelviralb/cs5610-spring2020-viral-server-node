ee37b702b2a29ac8f9bda3b12d3800afce4ee037
viralbpatel <patel.viral@husky.neu.edu>
2020-04-07 22:08:16 -0400

    Quiz, Questions and Attempt related files added

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(function (request, response, next) {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
-    /* response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); */
-    response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    /*response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');*/
     next();
 });
 
---
+++ models/questions/questions.models.server..js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const questionsSchema = require('./questions.schema.server');
+
+const questionsModel = mongoose.model(
+    'QuestionsModel',
+    questionsSchema
+);
+
+module.exports = questionsModel;
